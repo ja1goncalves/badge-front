bacb2826f5f020b2ae6e95a6bb15e5ad5795f196
joao paulo <joaosilva@mangue3.com>
2019-02-25 18:29:37 -0300

    imagem do cracha pelo input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'badge-front';
+  layout: File;
   atributtes = [
     {
       title: "Nome",
@@ -37,4 +40,19 @@ export class AppComponent {
       size: 15
     }
   ];
+
+  public getImageBadge(){
+    const input = document.getElementById("layout-badge") as HTMLInputElement;
+    this.layout = input.files[0];
+
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#blah').attr('src', e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
 }
